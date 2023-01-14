fa9c8bb2a8b559667cc0c418c923b7dd13d51bc8
Samuel Soyebo <ayodejisoyebo@gmail.com>
2023-01-14 21:46:30 +0100

    Sidebar Design

--- portfolio/src/Sidebar/index.js
+++ portfolio/src/Sidebar/index.js
@@ -5,8 +5,28 @@ function Sidebar() {
   return (
     <SidebarStyle>
       <div className="sidebar">
-        <div className="sidebar-right"></div>
-        <div className="sidebar-left"></div>
+        <div className="sidebar-right">
+          <div>
+            {" "}
+            <h1>git</h1>
+          </div>
+          <div>
+            {" "}
+            <h1>git</h1>
+          </div>
+          <div>
+            {" "}
+            <h1>git</h1>
+          </div>
+          <div>
+            {" "}
+            <h1>git</h1>
+          </div>
+        </div>
+              <div className="sidebar-left">
+                  <h3> <a href=''>ayodejisoyebo@gmail.com</a>
+                  </h3>
+        </div>
       </div>
     </SidebarStyle>
   );
